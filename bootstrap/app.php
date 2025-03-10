b74554b2356d882659bb1be11efa4ef9e187f950
Kamran Khan <kkhan2928@gmail.com>
2025-03-10 15:30:48 +0530

    Cors added2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->validateCsrfTokens(except: [
-            'botman' 
+            'botman',
+            'botman/*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
