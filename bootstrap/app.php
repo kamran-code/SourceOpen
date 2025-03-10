2cccba94614940732c2e34fad18cf9be878315e1
Kamran Khan <kkhan2928@gmail.com>
2025-03-10 15:21:51 +0530

    Csrf token fix2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->validateCsrfTokens(except: [
+            'botman' 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
