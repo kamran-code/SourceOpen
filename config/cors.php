c2907a4eec8c04e1415177d522076d268c47e3e4
Kamran Khan <kkhan2928@gmail.com>
2025-03-10 13:51:38 +0530

    Cors added

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5501'],
+    'allowed_origins' => ['http://127.0.0.1:5501','https://www.maksoft.in/'],
 
     'allowed_origins_patterns' => [],
 
