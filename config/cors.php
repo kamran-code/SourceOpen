49100fcb54d3e84ca9a16ad7aaeaca6b070ac656
Kamran Khan <kkhan2928@gmail.com>
2025-03-23 13:50:43 +0530

    Cors Updatec

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5501', 'https://www.maksoft.in', 'https://sourceopen.in'], // ✅ Add your domains
+    'allowed_origins' => ['http://127.0.0.1:5501', 'https://www.maksoft.in', 'https://sourceopen.in', 'https://chatbot.sourceopen.in'], // ✅ Add your domains
 
     'allowed_headers' => ['X-Requested-With', 'Content-Type', 'X-CSRF-TOKEN', '*'], // ✅ Allow CSRF Header
 
